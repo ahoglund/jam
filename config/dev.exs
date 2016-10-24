4c786326226f6451d3e13730df1ac07f4076491b
Andrew Hoglund <ahoglund@gmail.com>
2016-10-24 10:27:14 -0400

    secret dev db file

--- config/dev.exs
+++ config/dev.exs
@@ -32,11 +32,5 @@ config :logger, :console, format: "[$level] $message\n"
 # and calculating stacktraces is usually expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
-config :bad_mamma_jamma, BadMammaJamma.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "hoglundandrew",
-  password: "postgres",
-  database: "bad_mamma_jamma_dev",
-  hostname: "localhost",
-  pool_size: 10
+import_config "dev.secret.exs"
+
